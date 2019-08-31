c73d612d396174a34f570caf4b7bada8b00c3602
NguyenTheGiaHuy1 <trankimdung6479@gmail.com>
2019-08-31 21:11:22 +0700

    commit day tien

---
+++ Session01/hello_world.py
@@ -0,0 +1,18 @@
+print("Hello world")
+n = input("What's your name?")
+print("Hi")
+print(n)
+print("My name Huy")
+n=input("How are you?")
+print("I am fine too")
+n=input("How old are you?")
+print("I am 15 years old")
+
+
+
+
+
+
+
+
+
---
+++ Session02/index.py
@@ -0,0 +1,5 @@
+# ten = input("Moi ban nhap ten :")
+# print("Xin chao ban"+ten)
+so  = int(input("moi nguoi dung nhap so"))
+print(so+1)
+
